47e8f8108c371c7505f61ccd2aa3256fb02afa16
sepeca15 <sepeca15@gmail.com>
2021-04-27 19:45:54 +0000

    Div card + home

---
+++ src/js/component/DivCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Card = props => {
+	return (
+		<div className="card m-5">
+			<img
+				className="card-img-top"
+				src={props.imageUrl}
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">{props.description}</p>
+				<a href={props.buttonUrl} className="btn btn-primary">
+					{props.buttonLabel}
+				</a>
+			</div>
+		</div>
+	);
+};
+Card.propTypes = {
+	title: PropType.string,
+	imageUrl: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,58 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import NavbarNico from "./navbarNico";
+//import NavBar from "./navbarRosario";
+//import Jumbotron from "./Jumbotron.js";
+import Card from "./DivCard.js";
+//import Footer from "./footer.js";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			{/* <div id="NavBar">
+				<NavBar />
+			</div>
+			<div id="Jumbotron"></div> */}
+			<div id="DivCard container-fluid">
+				<Card
+					title="Valentina"
+					imageUrl="https://picsum.photos/200/300"
+					description="Son integrante del Segundo mejor grupo."
+					buttonUrl="https://en.wikipedia.org/wiki/Valentina"
+					buttonLabel="Go to wikipedia"
+				/>
+				<Card
+					title="Rosario"
+					imageUrl="https://picsum.photos/200/301"
+					description="Son integrante del Segundo mejor grupo."
+					buttonUrl="https://en.wikipedia.org/wiki/Rosario"
+					buttonLabel="Go to wikipedia"
+				/>
+				<Card
+					title="Natalia"
+					imageUrl="https://picsum.photos/200/302"
+					description="Son integrante del Segundo mejor grupo."
+					buttonUrl="https://en.wikipedia.org/wiki/Natalia"
+					buttonLabel="Go to wikipedia"
+				/>
+				<Card
+					title="Nicolas"
+					imageUrl="https://picsum.photos/200/303"
+					description="Son integrante del Segundo mejor grupo."
+					buttonUrl="https://en.wikipedia.org/wiki/Nicolas"
+					buttonLabel="Go to wikipedia"
+				/>
+				<Card
+					title="Seba"
+					imageUrl="https://picsum.photos/200/304"
+					description="Son integrante del Segundo mejor grupo."
+					buttonUrl="https://en.wikipedia.org/wiki/Seba"
+					buttonLabel="Go to wikipedia"
+				/>
+			</div>
+			<div id="Footer">
+				<footer />
+			</div>
 		</div>
 	);
 }
