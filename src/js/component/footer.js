cf01ef555a075b766407cf5f494a4c1e9fdabed3
Valentina Cuello <delmar_vale08@hotmail.com>
2021-04-27 20:56:10 +0000

    Arreglé el footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Footer = () => {
 	return (
-		<footer>
-			<div className="container-fluid bg-dark">
-				<p className="text-white fw-bold">Copyright © - Grupo 6</p>
-			</div>
+		<footer
+			className="footer  bg-dark container-fluid d-flex justify-content-center align-items-center"
+			style="height: 150px">
+			<p className="text-white fw-bold">Copyright © - Grupo 6</p>
 		</footer>
 	);
 };
